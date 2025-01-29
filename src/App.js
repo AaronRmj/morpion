f9e75b4b26e48f75ce93529bebace1a1212a0290
senpai <aaronrmjk@gmail.com>
2025-01-29 22:46:42 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  //alternance
+  const [XisNext, setXisNext] = useState(true);
+
+  //board gère memoire interne anle app
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice(); //creation nouveau tab
+    if (XisNext) {
+      nextSquares[i] = "X";
+    } else nextSquares[i] = "O";
+
+    setSquares(nextSquares); // MAJ du valeur de squares
+    setXisNext(!XisNext); // O is next
+  }
+  return (
+    //tableaux de 9 elements valeurs null
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+}
